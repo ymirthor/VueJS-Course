523f55f485c225b5008014f163451327d3e12678
ymirthor <ymirthorleifsson@gmail.com>
2021-06-02 20:59:28 +0000

    section 34 half way there and major refactoring done

--- app.js
+++ app.js
@@ -4,54 +4,57 @@ new Vue({
     title: 'VueJS v2 & v3 Course',
     links: {
       'Section 23': {
-        'Hello World!': 'Section23/part1/index.html'
+        'Hello World!': 'Sections/Section23/part1/index.html'
       },
       'Section 24': {
-        'Using variables, functions & template': 'Section24/part1/index.html',
+        'Using variables, functions & template': 'Sections/Section24/part1/index.html',
         'Exercise 1': 'Exercises/Exercise1/index.html',
-        'Events & updating the DOM': 'Section24/part2/index.html',
+        'Events & updating the DOM': 'Sections/Section24/part2/index.html',
         'Exercise 2': 'Exercises/Exercise2/index.html',
-        'Updating variables with input field': 'Section24/part3/index.html',
-        'Computed & watched variables': 'Section24/part4/index.html',
-        'Shorter syntax for v-on: & v-bind:': 'Section24/part5/index.html',
+        'Updating variables with input field': 'Sections/Section24/part3/index.html',
+        'Computed & watched variables': 'Sections/Section24/part4/index.html',
+        'Shorter syntax for v-on: & v-bind:': 'Sections/Section24/part5/index.html',
         'Exercise 3': 'Exercises/Exercise3/index.html',
-        'Styling': 'Section24/part6/index.html',
+        'Styling': 'Sections/Section24/part6/index.html',
         'Exercise 4': 'Exercises/Exercise4/index.html'
       },
       'Section 25': {
-        'if, else & show': 'Section25/part1/index.html',
-        'Displaying arrays and objects': 'Section25/part2/index.html',
+        'if, else & show': 'Sections/Section25/part1/index.html',
+        'Displaying arrays and objects': 'Sections/Section25/part2/index.html',
         'Exercise 5': 'Exercises/Exercise5/index.html'
       },
       'Section 26': {
-        'Monster Slayer': 'Section26/index.html'
+        'Monster Slayer': 'Sections/Section26/index.html'
       },
       'Section 27': {
-        'Understanding lifespan of a vue instance': 'Section27/index.html'
+        'Understanding lifespan of a vue instance': 'Sections/Section27/index.html'
       },
       'Section 28': {
-        'Using development server': 'Section28/index.html'
+        'Using development server': 'Sections/Section28/index.html'
       },
       'Section 29': {
-        'Using components': 'Section29/part1/index.html',
-        'Why components use functions for data': 'Section29/part2/index.html',
-        'Local & glopal components': 'Section29/part3/index.html',
-        'Root component': 'Section29/part4/index.html',
+        'Using components': 'Sections/Section29/part1/index.html',
+        'Why components use functions for data': 'Sections/Section29/part2/index.html',
+        'Local & glopal components': 'Sections/Section29/part3/index.html',
+        'Root component': 'Sections/Section29/part4/index.html',
         'Exercise 6': 'Exercises/Exercise6/index.html'
       },
       'Section 30': {
         'Exercise 7': 'Exercises/Exercise7/index.html'
       },
       'Section 31': {
-        'Using slots for components': 'Section31/index.html',
+        'Using slots for components': 'Sections/Section31/index.html',
         'Exercise 8': 'Exercises/Exercise8/index.html'
       },
       'Section 32': {
-        'Quotes Quota': 'Section32/index.html',
+        'Quotes Quota': 'Sections/Section32/index.html',
       },
       'Section 33': {
-        'Form control': 'Section33/index.html',
+        'Form control': 'Sections/Section33/index.html',
         'Exercise 9': 'Exercises/Exercise9/index.html'
+      },
+      'Section 34': {
+        'Directives': 'Sections/Section34/index.html',
       },
       
     }
