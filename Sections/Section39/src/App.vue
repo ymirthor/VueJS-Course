d9c35252cf0829468ce796566c0d21edbb3d45b3
ymirthor <ymirthorleifsson@gmail.com>
2021-06-07 23:55:40 +0000

    Started on section 39

---
+++ Sections/Section39/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Vuex</h1>
+        <rud-result></rud-result>
+        <rud-another-result></rud-another-result>
+        <hr />
+        <rud-counter></rud-counter>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import rudCounter from "./components/Counter.vue";
+import rudResult from "./components/Result.vue";
+import rudAnotherResult from "./components/AnotherResult.vue";
+
+export default {
+  components: {
+    rudCounter,
+    rudResult,
+    rudAnotherResult,
+  },
+};
+</script>
+
